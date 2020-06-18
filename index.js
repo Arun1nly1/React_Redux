565e2432831c1d978bf9767fbe7196cb9c006897
Arun Chapagain <arunsansar123@gmail.com>
2020-06-18 18:39:59 +0545

    Redux core completed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 // import redux from 'redux';
 const redux = require("redux")
+const reduxLogger = require("redux-logger")
 
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
-
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_iceCream = 'BUY_iceCream'
@@ -83,13 +85,12 @@ const iceCreamReducer = (state = initialIceCreamState, action)=>{
      iceCream : iceCreamReducer
  })
 
- const store = createStore(rootReducer)
+ const store = createStore(rootReducer,applyMiddleware(logger))
 // const store = createStore(reducer)
 
 console.log('Initial state', store.getState())
 
-const unsubscribe = store.subscribe(()=>
-console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(buyCake())
 store.dispatch(buyCake())
